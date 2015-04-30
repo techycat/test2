5f60c2412478c6ea8b387889c1fb4c7b98bd2b9e
techycat <techycat7@gmail.com>
2015-04-30 23:56:20 +0930

    Create server.js

---
+++ server.js
@@ -0,0 +1,13 @@
+var http = require('http');
+
+// Configure our HTTP server to respond with Hello World to all requests.
+var server = http.createServer(function (request, response) {
+  response.writeHead(200, {"Content-Type": "text/plain"});
+  response.end("Hai this is Nithin\n");
+});
+
+var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
+var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
+ 
+server.listen(server_port, server_ip_address, function () {
+ });
